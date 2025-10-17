37d7d7a9b5f91aa62d8005ad40935850f8fe4948
Jason Duquenne <47518779+Jduquenne@users.noreply.github.com>
2025-10-17 14:04:35 +0200

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dofus-craft-dashboard/", // Remplacer par le nom de votre repo
+  base: "/dofus-retro-craft-dashboard/", // Remplacer par le nom de votre repo
 });
