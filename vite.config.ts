5b06d2ff551adf73407eb39d37956c94171fd09c
Jason Duquenne <47518779+Jduquenne@users.noreply.github.com>
2025-10-23 13:43:51 +0200

    fix tailwind problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dofus-retro-craft-dashboard/", // Remplacer par le nom de votre repo
+  base: "/dofus-retro-craft-dashboard/", // ⚠️ DOIT correspondre EXACTEMENT au nom de votre repo GitHub
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
 });
